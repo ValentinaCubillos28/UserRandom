fc3ad3d080853856a09c6e422777b60f6fa5e350
Valentina Cubillos <lauracubi1063@gmail.com>
2025-05-30 00:47:13 +0000

    app.js

---
+++ metro.config.js
@@ -0,0 +1,5 @@
+const { getDefaultConfig } = require('@expo/metro-config');
+const config = getDefaultConfig(__dirname);
+// Soluciona problemas con .cjs y Firebase
+config.resolver.sourceExts.push('cjs');
+module.exports = config;
